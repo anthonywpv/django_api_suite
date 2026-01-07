58c5c70936ad83ee9a197d0fac9693d1ad578a40
Anthony Paredes <anthonyparedes1080@gmail.com>
2026-01-07 16:13:10 +0000

    Proyect 6 | Guide 21 Part 2

--- demo_rest_api/views.py
+++ demo_rest_api/views.py
@@ -39,4 +39,48 @@ class DemoRestApi(APIView):
       return Response({'message': 'Dato guardado exitosamente.', 'data': data}, status=status.HTTP_201_CREATED)
 
 
+    def put(self, request):
+      data = request.data
+
+      if 'id' not in data:
+        return Response({'error': 'Debe especificar al menos el ID'}, status=status.HTTP_400_BAD_REQUEST)
+
+      for i in data_list:
+        if i.get('id') == data.get('id'):
+          i['name'] = data.get('name', '')
+          i['email'] = data.get('email', '')
+          i['is_active'] = data.get('is_active', True)
+          return Response({'message': 'Dato actualizado exitosamente.', 'data': data}, status=status.HTTP_200_OK)
+        
+      return Response({'mesage': 'ID no encontrado', 'data': data}, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request):
+      data = request.data
+
+      if 'id' not in data:
+        return Response({'error': 'Debe especificar al menos el ID'}, status=status.HTTP_400_BAD_REQUEST)
+
+      for i in data_list:
+        if i.get('id') == data.get('id'):
+          i['name'] = data.get('name', i['name'])
+          i['email'] = data.get('email', i['email'])
+          i['is_active'] = data.get('is_active', i['is_active'])
+          return Response({'message': 'Dato cambiado exitosamente.', 'data': data}, status=status.HTTP_200_OK)
+
+      return Response({'message': 'ID no encontrado', 'data': data}, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request):
+      data = request.data
+
+      if 'id' not in data:
+        return Response({'error': 'Debe agregar el campo ID'}, status=status.HTTP_400_BAD_REQUEST)
+
+      for i in data_list:
+        if i.get('id') == data.get('id'):            
+          data_list.remove(i)
+      
+      return Response({'message': 'Dato eliminado exitosamente', 'data': data}, status=status.HTTP_200_OK)
+            
+
+
     
